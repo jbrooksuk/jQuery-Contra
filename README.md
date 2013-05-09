3d36cb282f5697620eae7d8134e49d572f0457bd
James Brooks <ukjbrooks@gmail.com>
2013-05-09 09:25:54 +0100

    Plugin changes. README update.
    Added more callbacks. Tidied formatting. Updated README. Updated jQuery
    Version.

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
-jQuery Contra code
-==================
+# jQuery Contra Code
+Easily create customizable Contra Code style Easter eggs in your web application.
 
-By James Brooks (http://www.jbrooksuk.eu)
+# Usage
+Getting up and running is really easy!
 
-The jquery.contra.js allows you to define (built-in) a set of key codes that must be sequentially pressed to activate the callback function.
+    $.contra(config);
 
-Options
--------
+# Example
+	
+	$.contra({
+		begin: function() {
+			console.log("The user has begun entering the contra code.");
+		},
+		fail: function() {
+			console.log("Fail!");
+		},
+		success: function() {
+			alert("You're a genius!");
+		}
+	});
 
-jquery.contra.js contains a few options which allow you to customize many aspects of it.
+# Configuration
+jQuery Contra contains a few options which allow you to customize many aspects of it.
 
-* options.scope - which sets where the callback is scoped from
-* options.code - an array of keycodes which the user is required to press in sequential order for the callback to be made
-* options.time - the time (in milliseconds) the user has ***free*** between each key press, if you don't want to reset the keycode if the user fails to press the next key, set this to false.
+- `config.scope` - The scope in which to bind key presses to.
+- `config.code`  - Array of keycodes to step through.
+- `config.time`  - The amount of time in milliseconds that the user must press the next key in the sequence. Optionally, if there is no timeout, use `false`.
+
+jQuery Contra also supports a few optional callback functions. These are useful in a couple of scenarios:
+
+1. Tracking successful Contra codes.
+2. Tracking the fail rate.
+3. Performing actions before the contra code is completed/failed.
+
+The callback functions are:
+
+- `config.fail`    - Called when the user has started entering the Contra code, but failed to hit the next key in the sequence correctly.
+- `config.success` - After the keycodes have been successfully entered, this will be called.
+- `config.begin`   - This is called after the user hits the first key in the sequence.
+
+# License
+MIT [http://jbrooksuk.mit-license.org](http://jbrooksuk.mit-license.org)
--- jquery.contra.js
+++ jquery.contra.js
@@ -1,62 +1,62 @@
 /*
- *
- *      jQuery Conta Code (jquery.contra.js)
- *      Konami Contra code implemented into jQuery
- *      (http://en.wikipedia.org/wiki/Konami_Code)
- *
- *      Copyright (c) 2010 James Brooks (http://www.jbrooksuk.eu)
- *
- *      Uses jQuery JavaScript library (http://www.jquery.com)
- *  
+* jQuery Conta Code (jquery.contra.js)
+* Konami Contra code implemented in jQuery
+* (http://en.wikipedia.org/wiki/Konami_Code)
+*
+* @author James Brooks <jbrooksuk@me.com>
+* @website http://james.brooks.so
+* @license MIT - http://jbrooksuk.mit-license.org
 */
 
-( function( $ ) {
+(function($) {
+	$.contra = function(config) {		
+		var currStep = 0, pressKey, timerStarted = false, keyTimer;
+		
+		config         = config || {};
+		config.begin   = config.begin || false;
+		config.success = config.success || false;
+		config.scope   = config.scope || $(document);
+		config.code    = config.code || [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]; // Up, Up, Down, Down, Left, Right, Left, Right, B, A, Enter
+		config.time    = config.time || 3000; // Time in ms or false
+		config.fail = config.fail || false;
 
-	$.contra = function( callback, options ) {
-		if ( typeof callback != 'function' ) return false;
-        
-        var currStep = 0, pressKey;
-        var timerStarted = false, keyTimer;
-        
-		options = options || {};
-		options.scope = options.scope || $( document );
-		options.code = options.code || [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
-        options.time = options.time || 2500; // Milliseconds or false
+		config.scope.keyup(function(e) {
+			pressKey = e.which || e.keyCode;
 
-		options.scope.keyup( function( e ) {
-            pressKey = e.which || e.keyCode;
-
-            if ( pressKey === options.code[currStep] ) {            
-                if ( !timerStarted && options.time != false ) {
-                    keyTimer = setTimeout(function( time ) {
-                        timerStarted = false;
-                        clearTimeout( keyTimer );
-                        currStep = 0;
-                    }, options.time );
-                }
-                
-                currStep++;
-                timerStarted = true;
-                
-                if ( currStep >= options.code.length ) {
-                    callback( );
-                    timerStarted = false;
-                    clearTimeout( keyTimer );
-                    currStep = 0;
-                }
-            }else{
-                currStep = 0;
-                timerStarted = false;
-                clearTimeout( keyTimer );
-            }
+			if(pressKey === config.code[currStep]) {
+				if(!timerStarted && config.time !== false) {
+					if(typeof config.begin === "function") config.begin();
+					keyTimer = setTimeout(function(time) {
+						timerStarted = false;
+						clearTimeout(keyTimer);
+						currStep = 0;
+					}, config.time);
+				}
+				
+				currStep++;
+				timerStarted = true;
+				
+				if(currStep >= config.code.length) {
+					console.log("Success");
+					if(typeof config.success === "function") config.success();
+					timerStarted = false;
+					clearTimeout(keyTimer);
+					currStep = 0;
+				}
+			}else{
+				// Call the timeout function
+				if(typeof config.fail === "function") config.fail();
+				currStep     = 0;
+				timerStarted = false;
+				clearTimeout(keyTimer);
+			}
 		});
 	};
 
-	$.fn.contra = function( callback, options ) {
-		var options = options || {};
-		options.scope = $( this );
+	$.fn.contra = function(config) {
+		var config = config || {};
+		config.scope = $(this);
 
-		$.contra( callback, options );
+		$.contra(config);
 	};
-
-} )( jQuery );
+})(jQuery);
